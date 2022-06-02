94332376209240c975f9a1efdc73c00651781b37
Adriana Gavrilut <adriana.gavrilut21@gmail.com>
2022-06-02 17:27:37 +0300

    Add project files.

---
+++ Test_BigBall/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace BigBallGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDrawCircle = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // buttonDrawCircle
+            // 
+            this.buttonDrawCircle.Location = new System.Drawing.Point(13, 13);
+            this.buttonDrawCircle.Name = "buttonDrawCircle";
+            this.buttonDrawCircle.Size = new System.Drawing.Size(75, 23);
+            this.buttonDrawCircle.TabIndex = 0;
+            this.buttonDrawCircle.Text = "Draw circle";
+            this.buttonDrawCircle.UseVisualStyleBackColor = true;
+            this.buttonDrawCircle.Click += new System.EventHandler(this.buttonDrawCircle_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(95, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(796, 406);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 433);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonDrawCircle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDrawCircle;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Test_BigBall/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BigBallGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics g;
+
+        Tuple<int, int, int, int> bila;
+        Random rndX;
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            rndX = new Random();
+        }
+
+        private void buttonDrawCircle_Click(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            bila = new Tuple<int, int, int, int>(250, 300, 15, 15);
+            Timer myTimer = new Timer();
+            myTimer.Interval = 20;
+            myTimer.Tick += new System.EventHandler(Draw);
+            //myTimer.Elapsed += OnTimedEvent;
+            //myTimer.AutoReset = true;
+            myTimer.Enabled = true;
+
+
+        }
+
+        private void Draw(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            panel1.Refresh();
+            Pen pen = new Pen(Color.Black, 2);
+            g.DrawEllipse(pen, bila.Item1, bila.Item2, bila.Item3, bila.Item4);
+
+            
+            bila = new Tuple<int, int, int, int>(rndX.Next(bila.Item1 - 2, bila.Item1 + 2), rndX.Next(bila.Item2 - 2, bila.Item2 + 2), 15, 15);
+
+        }
+    }
+}
